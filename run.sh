95124f685e482390dd60af9ac01a82bec17f498b
Zen <zhengxu5511@gmail.com>
2018-02-27 21:21:39 +0000

    add files

--- run.sh
+++ run.sh
@@ -2,11 +2,11 @@ PWD=$(pwd)
 parseDocument="ruby $PWD/target/classes/cran/split_document.rb"
 eval $parseDocument
 answer="$PWD/target/classes/cran/parsed_answer"
-searchDocument="java -jar target/picasso-1.0-SNAPSHOT-jar-with-dependencies.jar"
+searchDocument="java -jar picasso-1.0-SNAPSHOT.jar"
 eval $searchDocument
 results="$PWD/target/classes/cran/test_results"
 cd "trec_eval"
 evaluate="./trec_eval $answer $results"
 eval $evaluate
-#deleteAnswer="rm $answer && rm $results"
-#eval $deleteAnswer
+deleteAnswer="rm $answer && rm $results"
+eval $deleteAnswer
